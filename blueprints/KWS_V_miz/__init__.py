cab6d36cd6c567cb9afb9b9c1b0de3b314940327
Souvik Banik <145757901+souvik0602@users.noreply.github.com>
2026-01-15 11:38:15 +0530

    added-2

---
+++ blueprints/KWS_V_miz/__init__.py
@@ -0,0 +1,8 @@
+"""
+KWS_V_bng package initializer.
+
+Creates a package marker so Python treats the folder as a package for imports
+like `from .KWS_V_bng.KWS_prediction import predict_keyword`.
+"""
+
+__all__ = ["KWS_prediction", "KWS_denseNet_bng", "speechproc", "read_writehtk"]
